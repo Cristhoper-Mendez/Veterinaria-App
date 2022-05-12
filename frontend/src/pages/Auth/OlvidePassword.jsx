a91ce44a3313baa15512512dc3cf48702ed480a5
Cristhoper Mendez <odito2009@gmail.com>
2022-05-12 17:01:02 -0600

    Olvide password.

--- frontend/src/pages/Auth/OlvidePassword.jsx
+++ frontend/src/pages/Auth/OlvidePassword.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+
+import { Link } from "react-router-dom";
 
 const OlvidePassword = () => {
-    return (
-        <>
-            Desde olvide password
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Recuepera tu Acceso y no Pierdas {""}
+          <span className="text-black">tus Pacientes</span>
+        </h1>
+      </div>
 
-export default OlvidePassword
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        <form>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Email
+            </label>
+            <input
+              type="email"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              placeholder="Tu email"
+            />
+          </div>
+        </form>
+      </div>
+
+      <input
+        type="submit"
+        value="Enviar instrucciones"
+        className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+      />
+
+      <nav className="mt-10 lg:flex lg:justify-between">
+        <Link to="/registrar" className="block text-center my-5 text-gray-500">
+          ¿Ya tienes una cuenta? Inicia sesion!
+        </Link>
+        <Link
+          to="/registrar"
+          className="block text-center my-5 text-gray-500"
+        >
+          ¿No tienes una cuenta? Registrate ya!
+        </Link>
+      </nav>
+    </>
+  );
+};
+
+export default OlvidePassword;
