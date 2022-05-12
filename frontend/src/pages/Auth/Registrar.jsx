e2c8ffc7d236ec17634c77004e7ed652c57c5c5c
Cristhoper Mendez <odito2009@gmail.com>
2022-05-12 16:33:19 -0600

    Vista de registrar y pequeñas correcciones.

--- frontend/src/pages/Auth/Registrar.jsx
+++ frontend/src/pages/Auth/Registrar.jsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import React from "react";
+
+import { Link } from "react-router-dom";
 
 const Registrar = () => {
-    return (
-        <>
-            Desde registrar
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Crea tu Cuenta y Administra {""}
+          <span className="text-black">tus Pacientes</span>
+        </h1>
+      </div>
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        <form>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Nombre
+            </label>
+            <input
+              type="text"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              placeholder="Tu nombre"
+            />
+          </div>
 
-export default Registrar
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Email
+            </label>
+            <input
+              type="email"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              placeholder="Email de login"
+            />
+          </div>
+
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              placeholder="Tu password"
+            />
+          </div>
+
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Repetir Password
+            </label>
+            <input
+              type="password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              placeholder="Repite tu password"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Iniciar sesion"
+            className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+          />
+        </form>
+
+        <nav className="mt-10 lg:flex lg:justify-between">
+          <Link
+            to="/registrar"
+            className="block text-center my-5 text-gray-500"
+          >
+            ¿Ya tienes una cuenta? Inicia sesion!
+          </Link>
+          <Link
+            to="/registrar"
+            className="block text-center my-5 text-gray-500"
+          >
+            Olvide mi Password
+          </Link>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Registrar;
