3f02105b365114664a04f0382b808caac83a194d
Cristhoper Mendez <odito2009@gmail.com>
2022-07-09 18:01:14 -0600

    Iniciar sesión y AuthContext.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,18 +8,22 @@ import OlvidePassword from "./pages/Auth/OlvidePassword";
 import Registrar from "./pages/Auth/Registrar";
 import NuevoPassword from "./pages/Auth/NuevoPassword";
 
+import { AuthProvider } from "./Context/AuthProvider";
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path="confirmar-cuenta/:id" element={<ConfirmarCuenta />} />
-          <Route path="olvide-password" element={<OlvidePassword />} />
-          <Route path="olvide-password/:token" element={<NuevoPassword />} />
-          <Route path="registrar" element={<Registrar />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path="confirmar-cuenta/:id" element={<ConfirmarCuenta />} />
+            <Route path="olvide-password" element={<OlvidePassword />} />
+            <Route path="olvide-password/:token" element={<NuevoPassword />} />
+            <Route path="registrar" element={<Registrar />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,7 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import useAuth from "../../hooks/useAuth";
+
+import Alerta from "../../components/Alerta";
+
+import clienteAxios from "../../config/axios";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alerta, setAlerta] = useState({});
+
+  const { auth, setAuth } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (email.trim() === "" || password.trim() === "") {
+      return setAlerta({
+        msg: "Todos los campos son obligatorios",
+        error: true,
+      });
+    }
+
+    try {
+      const { data } = await clienteAxios.post("/veterinarios/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", data.token);
+      
+      navigate("/admin");
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <div>
@@ -11,7 +53,8 @@ const Login = () => {
         </h1>
       </div>
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        <form>
+        {msg && <Alerta alerta={alerta} />}
+        <form onSubmit={handleSubmit}>
           <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold">
               Email
@@ -20,6 +63,8 @@ const Login = () => {
               type="email"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
               placeholder="Email de login"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="my-5">
@@ -30,6 +75,8 @@ const Login = () => {
               type="password"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
               placeholder="Tu Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <input
