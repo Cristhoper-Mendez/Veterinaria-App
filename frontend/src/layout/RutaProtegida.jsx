f28300cef7801506987429bf32dcbad9aeb72540
Cristhoper Mendez <odito2009@gmail.com>
2022-07-17 16:44:04 -0600

    Footer terminado, header comenzado.

--- frontend/src/layout/RutaProtegida.jsx
+++ frontend/src/layout/RutaProtegida.jsx
@@ -3,6 +3,9 @@ import { Outlet, Navigate } from "react-router-dom";
 
 import useAuth from "../hooks/useAuth";
 
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 const RutaProtegida = () => {
   const { auth, cargando } = useAuth();
 
@@ -10,7 +13,11 @@ const RutaProtegida = () => {
 
   return (
     <>
+      <Header />
+
       {auth?._id ? <Outlet /> : <Navigate to="/" />}
+
+      <Footer />
     </>
   );
 };
