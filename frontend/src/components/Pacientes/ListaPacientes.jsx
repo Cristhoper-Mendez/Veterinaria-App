6b378f94aad6a9a2f215ce9b1b2d7fb67d7acc0e
Cristhoper Mendez <odito2009@gmail.com>
2022-12-10 17:41:38 -0600

    Se creo formulario para agregar paciente

---
+++ frontend/src/components/Pacientes/ListaPacientes.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const ListaPacientes = () => {
+    return ( <></> );
+}
+ 
+export default ListaPacientes;
--- frontend/src/pages/Pacientes/AdministrarPacientes.jsx
+++ frontend/src/pages/Pacientes/AdministrarPacientes.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+import Formulario from "../../components/Pacientes/Formulario";
+import ListaPacientes from "../../components/Pacientes/ListaPacientes";
+
 const AdministrarPacientes = () => {
   return (
     <>
-      <div>AdministrarPacientes</div>
+      <div className="flex flex-col md:flex-row">
+        <div className="md:w-1/2 lg:-2/5">
+          <Formulario />
+        </div>
+        <div className="md:w-1/2 lg:-2/5">
+          <ListaPacientes />
+        </div>
+      </div>
     </>
   );
 };
